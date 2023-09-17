276a171c89db25fd15ccd165b619d0e03b8fd474
baccrie <bakarerilwan1@gmail.com>
2023-09-17 11:14:19 +0100

    Task 2 corr d

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
 -- Task 2  Best band ever!
 -- A script that imports a sql dump file and Reads data to stdout
 
-SELECT origin AS origin, fans AS nb_fans from metal_bands ORDER BY nb_fans DESC;
+SELECT origin AS origin, SUM(fans) AS nb_fans from metal_bands GROUP BY origin ORDER BY nb_fans DESC;
