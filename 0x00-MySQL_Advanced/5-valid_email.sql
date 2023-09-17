67705087443b8cd6953fcf35515ed810bbf04bb5
baccrie <bakarerilwan1@gmail.com>
2023-09-17 17:16:03 +0100

    Task 5 corr1

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -6,10 +6,10 @@ CREATE TRIGGER email_validation BEFORE UPDATE
 ON users
 FOR EACH ROW
     BEGIN
-        IF NEW.email = OLD.email
+        IF NEW.email <> OLD.email
         THEN
-            SET NEW.valid_email = 1
-        END IF
+            SET NEW.valid_email = 0;
+        END IF;
     END
 $$
 DELIMETER ;
