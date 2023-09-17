48264851ad6a3a63cd60220eab57b9aab78bba93
baccrie <bakarerilwan1@gmail.com>
2023-09-17 17:31:51 +0100

    Corr

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,11 +1,11 @@
 -- Email validation to sent
 -- A script that validates the email_validation column after anu update
 
-DELIMETER $$
-CREATE TRIGGER email_trigger BEFORE UPDATE
-ON users
+DELIMITER $$ 
+CREATE TRIGGER email_trigger
+BEFORE UPDATE ON users
 FOR EACH ROW
-    BEGIN
+BEGIN
         IF NEW.email <> OLD.email
         THEN
             SET NEW.valid_email = 0;
