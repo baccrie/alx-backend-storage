07f693d0563c8e1cd25b6761f986cb80c14df640
baccrie <bakarerilwan1@gmail.com>
2023-09-17 10:12:51 +0100

    Task 1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,10 @@
--- 1. In and not out 
--- creates a table users
+-- Task 1 In and not out
+-- A script that creates a sql table users with conraints
+
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT, 
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL,
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL,
     PRIMARY KEY (id)
-);
+    );
