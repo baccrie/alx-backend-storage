a913098f360a7efeab356762c25c8c4f6e71a93e
baccrie <bakarerilwan1@gmail.com>
2023-09-17 16:57:31 +0100

    Task 4 corr

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,5 +1,5 @@
 -- Task 4  buy buy buy
--- A script that creates a trigger that decreases the quantity of an item after adding a new order.
+-- A script that creates a trigger that decreases the quantity of an item after adding a NEW order.
 DELIMETER $$
 CREATE TRIGGER update_items AFTER INSERT
 ON orders
@@ -7,8 +7,8 @@ FOR EACH ROW
 UPDATE items
     BEGIN
     
-    SET quantity = quantity + new.number
-    where name = new.item_name;
+    SET quantity = quantity - NEW.number
+    where name = NEW.item_name;
 
     END
 $$
