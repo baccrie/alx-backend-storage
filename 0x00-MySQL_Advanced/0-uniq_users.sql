b80507871957420c6946b056e8f7cb85da29a5b3
baccrie <bakarerilwan1@gmail.com>
2023-09-17 09:59:18 +0100

    Taskk 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,8 @@
 -- A script that creates a sql table users
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    PRIMARY KEY (id)
     );
