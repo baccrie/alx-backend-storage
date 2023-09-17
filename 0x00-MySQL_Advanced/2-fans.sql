8cdc06f3e6a05f503e7f82a2990ed49564101e12
baccrie <bakarerilwan1@gmail.com>
2023-09-17 10:48:12 +0100

    Task 2 corr

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,4 @@
 -- Task 2  Best band ever!
 -- A script that imports a sql dump file and Reads data to stdout
 
-CREATE TABLE IF NOT EXISTS users (
-    SELECT country as origin, fans as nb_fans from metal_bands ORDER BY fans DESC;
-    );
+SELECT country as origin, fans as nb_fans from metal_bands ORDER BY fans DESC;
