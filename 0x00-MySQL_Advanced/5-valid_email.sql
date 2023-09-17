9bd2720be20cc9800c940cf749c06eb570a4a9a7
baccrie <bakarerilwan1@gmail.com>
2023-09-17 17:19:25 +0100

    Task 5 corr1

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,15 +1,15 @@
 -- Email validation to sent
 -- A script that validates the email_validation column after anu update
 
-DELIMETER $$
-CREATE TRIGGER email_validation BEFORE UPDATE
-ON users
+DELIMITER $$ 
+CREATE TRIGGER email_trigger
+BEFORE UPDATE ON users
 FOR EACH ROW
-    BEGIN
-        IF NEW.email <> OLD.email
-        THEN
-            SET NEW.valid_email = 0;
-        END IF;
-    END
+BEGIN
+IF NEW.email <> OLD.email
+THEN
+    SET NEW.valid_email = 0;
+END IF;
+END
 $$
-DELIMETER ;
+DELIMITER ;
